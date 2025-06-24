6ead10744300d4c6fc269f3bf2b41a4ace647fbf
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2025-06-24 07:52:45 +0000

    adapt paths, try fix dependencies

--- .vercel/output/functions/api.func/core-app/jest.config.js
+++ .vercel/output/functions/api.func/core-app/jest.config.js
@@ -3,19 +3,11 @@ export default {
   transform: {},
   testEnvironment: "jsdom",
   coverageProvider: "v8",
-  setupFiles: [
-    "<rootDir>/.vercel/output/functions/api.func/core-app/tests/setup.jest.js",
-  ],
-  testPathIgnorePatterns: [
-    "<rootDir>/.vercel/output/functions/api.func/core-app/node_modules/",
-    "<rootDir>/.vercel/output/functions/api.func/core-app/tests/e2e/",
-  ],
-  modulePathIgnorePatterns: [
-    "<rootDir>/.vercel/output/functions/api.func/core-app/node_modules/",
-    "<rootDir>/.vercel/output/functions/api.func/core-app/tests/e2e/",
-  ],
+  setupFiles: ["<rootDir>/tests/setup.jest.js"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/tests/e2e/"],
+  modulePathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/tests/e2e/"],
   coveragePathIgnorePatterns: [
-    "<rootDir>/.vercel/output/functions/api.func/core-app/node_modules/",
-    "<rootDir>/.vercel/output/functions/api.func/core-app/tests/E2E/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/tests/E2E/",
   ],
 };
