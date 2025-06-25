51fc0b909803e742e70cc127fd540d0eed18b30e
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2025-06-25 16:56:00 +0000

    rename dot_vercel_copy, log requires req and res keys

--- _dot_vercel_copy/output/functions/api.func/router.js
+++ _dot_vercel_copy/output/functions/api.func/router.js
@@ -1,11 +1,24 @@
 import { default as gist } from "./api/gist.js";
 import { default as pin } from "./api/pin.js";
-import axios from "axios";
 
 export default async (req, res) => {
-  console.log(axios);
   const url = new URL(req.url, "https://localhost");
   req.query = Object.fromEntries(url.searchParams.entries());
+
+  console.log(
+    req.query +
+      " # " +
+      req.method +
+      " # " +
+      req.headers +
+      " # " +
+      res.setHeader +
+      " # " +
+      res.send +
+      " # " +
+      res.status,
+  );
+
   switch (url.pathname) {
     case "/gist":
       gist(req, res);
