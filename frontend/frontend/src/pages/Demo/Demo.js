3e44dcf8e9a4da401ccce5b78fab30936ff5eae7
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2025-09-20 13:46:17 +0200

    fix Demo again

--- frontend/frontend/src/pages/Demo/Demo.js
+++ frontend/frontend/src/pages/Demo/Demo.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Button, SvgInline } from '../../components';
-import { getValidUser } from '../../api/wrapped';
 import { HOST } from '../../constants';
 import { classnames } from '../../utils';
 
