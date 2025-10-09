aa3442f0b9e6403e8b5a65efb491bc6072eeaf8f
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2025-10-09 09:03:01 +0200

    move hard-coded env vars to Home.js

--- frontend/frontend/src/dotenv-browser-stub.js
+++ frontend/frontend/src/dotenv-browser-stub.js
@@ -1,10 +1,4 @@
 // Safe browser stub for dotenv
 export function config() {
-  window.process = {
-    env: {
-      PAT_1: 'myDummyPAT',
-    },
-  };
-  console.log('env var has been set');
   return { parsed: {} };
 }
--- frontend/frontend/src/pages/Home/Home.js
+++ frontend/frontend/src/pages/Home/Home.js
@@ -140,6 +140,11 @@ const HomeScreen = () => {
             });
             const res = createMockRes();
             try {
+              window.process = {
+                env: {
+                  PAT_1: 'myDummyPAT',
+                },
+              };
               await router(req, res);
               const body = res._getBody();
               const status = res._getStatusCode();
