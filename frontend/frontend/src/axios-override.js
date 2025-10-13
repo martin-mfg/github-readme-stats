da1d21cda9e9ff96e27d88e934ace0a8a5bc555f
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2025-10-13 19:33:03 +0200

    interpretHeader=false

--- frontend/frontend/src/axios-override.js
+++ frontend/frontend/src/axios-override.js
@@ -4,6 +4,7 @@ import { setupCache } from 'axios-cache-interceptor';
 const cachedAxios = setupCache(axios, {
   // Cache for 30 minutes
   ttl: 30 * 60 * 1000,
+  interpretHeader: false,
   methods: ['get', 'post'],
 });
 
