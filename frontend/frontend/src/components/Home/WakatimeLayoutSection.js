6464e28f658ad4eb6ef622131b4889e53c1d42a4
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2025-11-06 09:49:58 +0100

    prettier

--- frontend/frontend/src/components/Home/WakatimeLayoutSection.js
+++ frontend/frontend/src/components/Home/WakatimeLayoutSection.js
@@ -15,7 +15,12 @@ const WakatimeLayoutSection = ({ selectedOption, setSelectedOption }) => {
   const options = [
     DEFAULT_OPTION,
     { id: 2, label: 'Compact', disabled: false, value: 'compact' },
-    { id: 3, label: 'Text Only', disabled: false, value: 'default=hide_progress=true&card_width=315' },
+    {
+      id: 3,
+      label: 'Text Only',
+      disabled: false,
+      value: 'default=hide_progress=true&card_width=315',
+    },
   ];
 
   return (
